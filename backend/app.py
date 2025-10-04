e00901830d0eb25ec35c7660de3a0ab5d7227971
mammumammi <ashinmammu@gmail.com>
2025-10-04 20:25:26 +0530

    changed backend port number

--- backend/app.py
+++ backend/app.py
@@ -42,4 +42,5 @@ def upload():
     return jsonify(extracted_data)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides the port
+    app.run(host="0.0.0.0", port=port, debug=True)
